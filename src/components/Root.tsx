c54d8bf22adaa61586725cd3926de133737f1e81
dearsecret <dearsecret1217@gmail.com>
2022-12-19 15:23:04 +0900

    Input

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,6 +1,7 @@
-import { Button, HStack, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text, useDisclosure, VStack } from "@chakra-ui/react";
+import { Box, Button, HStack, Input, InputGroup, InputLeftElement, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text, useDisclosure, VStack } from "@chakra-ui/react";
 import { Link, Outlet } from "react-router-dom";
 import { BsBuilding} from "react-icons/bs"
+import { FaUser, FaLock} from "react-icons/fa"
 
 export default function Root(){
 
@@ -18,7 +19,7 @@ export default function Root(){
                     <Button bg="black" color="white" onClick={onOpen}>
                         로그인    
                     </Button>
-                    <Button variant={"ghost"}>
+                    <Button >
                         회원가입
                     </Button>
                 </HStack>
@@ -30,8 +31,24 @@ export default function Root(){
                         <ModalHeader>
                             로그인
                         </ModalHeader>
-                        <ModalBody>
-                            인풋창
+                        <ModalBody mb={15}>
+                            <VStack mb={15}>
+                                <InputGroup>
+                                    <InputLeftElement children={<Box color={"gray.500"}>
+                                        <FaUser />
+                                    </Box>}/>
+                                    <Input variant={"filled"} placeholder="아이디"/>
+                                </InputGroup>
+                                <InputGroup>
+                                    <InputLeftElement children={<Box color={"gray.500   "}>
+                                        <FaLock />
+                                    </Box>}/>
+                                    <Input variant={"filled"} placeholder="비밀번호" type={"password"}/>
+                                </InputGroup>
+                            </VStack>
+                            <Button w="100%" bg="black" color="white">
+                                로그인
+                            </Button>
                         </ModalBody>
                     </ModalContent>
                 </Modal>
